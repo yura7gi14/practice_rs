9b45cd5fb955853dc9fa2a60a04314fe46e5f5e0
yura7gi14 <yui.shiopi@gmail.com>
2023-05-29 13:45:12 +0900

    add:lec01

---
+++ lec01/work01-1/target/debug/deps/work01_1-9d9dd7af3f3259f7.d
@@ -0,0 +1,5 @@
+/home/yuragi/workplace/practice_rs/lec01/work01-1/target/debug/deps/work01_1-9d9dd7af3f3259f7: src/main.rs
+
+/home/yuragi/workplace/practice_rs/lec01/work01-1/target/debug/deps/work01_1-9d9dd7af3f3259f7.d: src/main.rs
+
+src/main.rs:
---
+++ lec01/work01-1/target/debug/work01-1.d
@@ -0,0 +1,1 @@
+/home/yuragi/workplace/practice_rs/lec01/work01-1/target/debug/work01-1: /home/yuragi/workplace/practice_rs/lec01/work01-1/src/main.rs
